2765157a43a998aa0e622449dfad63376dfb8753
Dinesh A <adepu.dinesh.a@gmail.com>
2017-04-27 14:02:06 +0530

    Self code to similate 8 bench marks. Sphere collision works. Wall collision has force problem

---
+++ src/dem/chung_8_benchmarks/simple_dem_c_to_python/grains.py
@@ -0,0 +1,105 @@
+from global_values import dt, grav, nu, mu
+import numpy as np
+
+
+def prediction(grains):
+    for g in grains:
+        if g.fixed is False:
+            g.x += dt * g.vx + 0.5 * dt * dt * g.ax
+            g.y += dt * g.vy + 0.5 * dt * dt * g.ay
+            g.vx += 0.5 * dt * g.ax
+            g.vy += 0.5 * dt * g.ay
+
+            g.ang += dt * g.angv + 0.5 * dt * dt * g.anga
+            g.angv += 0.5 * dt * g.anga
+
+        # Zero forces
+        g.fx = 0
+        g.fy = 0.0
+        g.t = 0.0
+        g.p = 0.0
+
+
+def interparticle_force(g, a, b):
+    if a > b:
+        # particle centre coordinate component differences
+        x_ab = g[b].x - g[a].x
+        y_ab = g[b].y - g[a].y
+
+        # Particle centre distance
+        dist = np.sqrt(x_ab * x_ab + y_ab * y_ab)
+
+        # size of overlap
+        dn = (g[a].R + g[b].R) - dist
+        if dn > 0:
+            xn = x_ab / dist
+            yn = y_ab / dist
+            xt = -yn
+            yt = xn
+
+            # compute the velocity of the contact
+            vx_ab = g[a].vx - g[b].vx
+            vy_ab = g[a].vy - g[b].vy
+            print(vx_ab)
+            print(vy_ab)
+            vn = vx_ab * xn + vy_ab * yn
+            vt = vx_ab * xt + vy_ab * yt - (g[a].R * g[a].angv + g[b].R *
+                                            g[b].angv)
+
+            # find contact parameters
+            r_eff = (g[a].R * g[b].R) / (g[a].R + g[b].R)
+            m_eff = (g[a].m * g[b].m) / (g[a].m + g[b].m)
+            tmp_1 = (1. - g[a].nu * g[a].nu) / g[a].E
+            tmp_2 = (1. - g[b].nu * g[b].nu) / g[b].E
+            E_eff = 1. / (tmp_1 + tmp_2)
+
+            kn = 4./3. * np.sqrt(r_eff) * E_eff
+            kt = 2./7. * kn
+
+            # Compute force in local axes
+            fn = -kn * dn**(3./2.) - nu * vn
+
+            # rotation
+            ft = abs(kt * vt)
+            if ft > mu * fn:
+                ft = mu * fn
+            if vt > 0:
+                ft = -ft
+
+            # calculate sum of forces on a and b in global coordinates
+            g[a].fx += fn * xn
+            g[a].fy += fn * yn
+            g[a].t += -ft * g[a].R
+            g[a].p += fn
+            g[b].fx -= fn * xn
+            g[b].fy -= fn * yn
+            g[b].p += fn
+            g[b].t += -ft * g[b].R
+
+
+def interact_grains(grains):
+    # loop through particle a
+    for a in range(len(grains)):
+        # Loop through particle b
+        for b in range(len(grains)):
+            interparticle_force(grains, a, b)
+
+
+def update_acc(grains):
+    for g in grains:
+        g.ax = g.fx / g.m
+        g.ay = g.fy / g.m
+        g.anga = g.t / g.I
+
+
+def apply_gravity(grains):
+    for g in grains:
+        g.ay += - grav
+
+
+def correction(grains):
+    for g in grains:
+        if g.fixed is False:
+            g.vx += 0.5 * dt * g.ax
+            g.vy += 0.5 * dt * g.ay
+            g.angv += 0.5 * dt * g.anga
---
+++ src/dem/chung_8_benchmarks/simple_dem_c_to_python/headers.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class Particle(object):
+    """Documentation for Particle
+
+    """
+
+    def __init__(self, R=0, x=0, y=0, ang=0, vx=0, vy=0, angv=0, ax=0, ay=0,
+                 anga=0, fx=0, fy=0, t=0, p=0, rho=0, nu=0.2, E=4.8 * 1e10,
+                 fixed=False):
+        super(Particle, self).__init__()
+        self.R = R
+        self.m = 4./3. * np.pi * rho * R * R * R
+        # self.m = np.pi * rho * R * R
+        self.I = 0.5 * self.m * R * R
+        self.x = x
+        self.y = y
+        self.ang = ang
+        self.vx = vx
+        self.vy = vy
+        self.angv = angv
+        self.ax = ax
+        self.ay = ay
+        self.anga = anga
+        self.fx = fx
+        self.fy = fy
+        self.t = t
+        self.p = p
+        self.fixed = fixed
+        self.nu = nu
+        self.E = E
+
+
+class Wall(object):
+    """Documentation for Wall
+
+    """
+    def __init__(self, pos, E, nu):
+        super(Wall, self).__init__()
+        self.pos = pos
+        self.E = E
+        self.nu = nu
