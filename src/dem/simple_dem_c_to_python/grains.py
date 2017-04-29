aed1c95353d53746794b8fdc94b41a5619a5417d
Dinesh A <adepu.dinesh.a@gmail.com>
2017-04-30 01:13:44 +0530

    changed the direction of vectors and tangential force according to computational granular dynamics

--- src/dem/simple_dem_c_to_python/grains.py
+++ src/dem/simple_dem_c_to_python/grains.py
@@ -1,4 +1,4 @@
-from global_values import dt, grav, nu, mu
+from global_values import dt, grav
 import numpy as np
 
 
@@ -17,14 +17,13 @@ def prediction(grains):
         g.fx = 0
         g.fy = 0.0
         g.t = 0.0
-        g.p = 0.0
 
 
 def interparticle_force(g, a, b):
     if a > b:
         # particle centre coordinate component differences
-        x_ab = g[b].x - g[a].x
-        y_ab = g[b].y - g[a].y
+        x_ab = g[a].x - g[b].x
+        y_ab = g[a].y - g[b].y
 
         # Particle centre distance
         dist = np.sqrt(x_ab * x_ab + y_ab * y_ab)
@@ -32,17 +31,19 @@ def interparticle_force(g, a, b):
         # size of overlap
         dn = (g[a].R + g[b].R) - dist
         if dn > 0:
-            xn = x_ab / dist
-            yn = y_ab / dist
+            dist_eff = 1. / dist
+            xn = x_ab * dist_eff
+            yn = y_ab * dist_eff
             xt = -yn
             yt = xn
 
             # compute the velocity of the contact
             vx_ab = g[a].vx - g[b].vx
             vy_ab = g[a].vy - g[b].vy
-            vn = vx_ab * xn + vy_ab * yn
-            vt = vx_ab * xt + vy_ab * yt - (g[a].R * g[a].angv + g[b].R *
-                                            g[b].angv)
+
+            vn = -(vx_ab * xn + vy_ab * yn)
+            vt = vx_ab * xt + vy_ab * yt + g[a].R * g[a].angv - g[b].R * g[
+                b].angv
 
             # find contact parameters
             r_eff = (g[a].R * g[b].R) / (g[a].R + g[b].R)
@@ -51,27 +52,38 @@ def interparticle_force(g, a, b):
             tmp_2 = (1. - g[b].nu * g[b].nu) / g[b].E
             E_eff = 1. / (tmp_1 + tmp_2)
 
-            kn = 4./3. * np.sqrt(r_eff) * E_eff
-            kt = 2./7. * kn
+            kn = 4. / 3. * np.sqrt(r_eff) * E_eff
+            # kt = 2. / 7. * kn
+
+            _tmp = np.log(g[a].e)
+            print(_tmp)
+            alpha_1 = _tmp * np.sqrt(5. / (_tmp**2 + np.pi**2))
+            eta_tmp = alpha_1 * np.sqrt(m_eff * kn)
+            eta_n = eta_tmp * dn**(1. / 4.)
+            eta_t = 1./2. * eta_n
 
             # Compute force in local axes
-            fn = -kn * dn**(3./2.) - nu * vn
+            fn = kn * dn**(3. / 2.) + eta_n * vn
+            ft = -eta_t * vt
 
             # rotation
-            ft = abs(kt * vt)
-            if ft > mu * fn:
-                ft = mu * fn
-            if vt > 0:
-                ft = -ft
+            if (fn < 0):
+                print("fn is less than zero")
+                fn = 0
+
+            if ft < (-g[0].mu * fn):
+                ft = -g[0].mu * fn
+
+            if ft > (g[0].mu * fn):
+                ft = g[0].mu * fn
 
             # calculate sum of forces on a and b in global coordinates
-            g[a].fx += fn * xn
-            g[a].fy += fn * yn
-            g[a].t += -ft * g[a].R
-            g[a].p += fn
-            g[b].fx -= fn * xn
-            g[b].fy -= fn * yn
-            g[b].p += fn
+            g[a].fx += fn * xn - ft * yn
+            g[a].fy += fn * yn + ft * xn
+            g[a].t += g[a].R * ft
+
+            g[b].fx -= fn * xn + ft * yn
+            g[b].fy -= fn * yn - ft * xn
             g[b].t += -ft * g[b].R
 
 
@@ -92,7 +104,7 @@ def update_acc(grains):
 
 def apply_gravity(grains):
     for g in grains:
-        g.ay += - grav
+        g.ay += -grav
 
 
 def correction(grains):
